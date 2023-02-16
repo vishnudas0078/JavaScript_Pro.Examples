573c9c08aefa205cb6f6f26861db3b0b093a3195
Vishnu Das <121727351+vishnudas0078@users.noreply.github.com>
2023-02-16 23:24:36 +0530

    JavaScript_Programming_PS

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JavaScript_Programming_PS
+
+16/02/2023
+Hello guys here i'm starting practice for JavaScript Pragramming Language,
+You Can also do practive by watching my codes.
---
+++ index.js
@@ -0,0 +1,28 @@
+// 1.    Add Two Numbers
+
+// 1st methoud
+/*
+const num1 = 4;
+const num2 = 9;
+
+let sumofnums = num1 + num2;
+
+console.log("This is sum of " + num1 + " and " + num2 + " is = " + sumofnums);
+*/
+
+
+// 2nd methoud
+/*
+const num1 = 4;
+const num2 = 9;
+
+let sumofnums = num1 + num2;
+
+console.log(`This is sum of ${num1} and ${num2} is ${sumofnums}`);
+*/
+
+
+
+// 2.    Swap Two Variables
+
+// Coming soon.
